a0f6e38d6cfc1b40cde5d28d5fd161d54d2fe768
rachyoung <101233810+rachyoung@users.noreply.github.com>
2022-03-08 22:12:25 -0500

    Add files via upload

---
+++ GitHub.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class GitHub {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        int num1   = scnr.nextInt();
+        int num2   = scnr.nextInt();
+        int result = num1 + num2;
+    }
+}
